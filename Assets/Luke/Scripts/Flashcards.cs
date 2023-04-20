93d7487999eda5f12cc03304d98ec70d37085f4d
LukeHoney <2248102@swansea.ac.uk>
2023-04-20 17:44:43 +0100

    Luke: Added Script for correct and incorrect guesses in flashcard scene

---
+++ Assets/Luke/Scripts/Flashcards.cs
@@ -0,0 +1,385 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flashcards : MonoBehaviour
+{
+    public int IncorrectGuesses;
+
+    [Header("Eggs")]
+    public GameObject SpottedRayEgg;
+    public GameObject ThornbackRayEgg;
+    public GameObject SmallEyedRayEgg;
+    public GameObject UndulateRayEgg;
+    public GameObject CuckooRayEgg;
+    public GameObject BlondeRayEgg;
+    public GameObject StarrySkateEgg;
+    public GameObject BlueSkakeEgg;
+    public GameObject FlapperSkakeEgg;
+    public GameObject WhiteSkakeEgg;
+    public GameObject SmallspottedCatsharkEgg;
+    public GameObject NursehoundEgg;
+    public GameObject BlackmouthCatsharkEgg;
+
+    [Header("Correct panel")]
+    public GameObject CorrectSpottedRay;
+    public GameObject CorrectThornbackRay;
+    public GameObject CorrectSmallEyedRay;
+    public GameObject CorrectUndulateRay;
+    public GameObject CorrectCuckooRay;
+    public GameObject CorrectBlondeRay;
+    public GameObject CorrectStarrySkate;
+    public GameObject CorrectBlueSkake;
+    public GameObject CorrectFlapperSkake;
+    public GameObject CorrectWhiteSkake;
+    public GameObject CorrectSmallspottedCatshark;
+    public GameObject CorrectNursehound;
+    public GameObject CorrectBlackmouthCatshark;
+
+    [Header("Incorrect panel")]
+    public GameObject IncorrectSpottedRay;
+    public GameObject IncorrectThornbackRay;
+    public GameObject IncorrectSmallEyedRay;
+    public GameObject IncorrectUndulateRay;
+    public GameObject IncorrectCuckooRay;
+    public GameObject IncorrectBlondeRay;
+    public GameObject IncorrectStarrySkate;
+    public GameObject IncorrectBlueSkake;
+    public GameObject IncorrectFlapperSkake;
+    public GameObject IncorrectWhiteSkake;
+    public GameObject IncorrectSmallspottedCatshark;
+    public GameObject IncorrectNursehound;
+    public GameObject IncorrectBlackmouthCatshark;
+
+    [Header("Buttons")]
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+    public Button button10;
+    public Button button11;
+    public Button button12;
+    public Button button13;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IncorrectGuesses = 0;
+        allfalse();
+    }
+    void OnEnable()
+    {
+        //Register Button Events
+        button1.onClick.AddListener(() => buttonCallBack(button1));
+        button2.onClick.AddListener(() => buttonCallBack(button2));
+        button3.onClick.AddListener(() => buttonCallBack(button3));
+        button4.onClick.AddListener(() => buttonCallBack(button4));
+        button5.onClick.AddListener(() => buttonCallBack(button5));
+        button6.onClick.AddListener(() => buttonCallBack(button6));
+        button7.onClick.AddListener(() => buttonCallBack(button7));
+        button8.onClick.AddListener(() => buttonCallBack(button8));
+        button9.onClick.AddListener(() => buttonCallBack(button9));
+        button10.onClick.AddListener(() => buttonCallBack(button10));
+        button11.onClick.AddListener(() => buttonCallBack(button11));
+        button12.onClick.AddListener(() => buttonCallBack(button12));
+        button13.onClick.AddListener(() => buttonCallBack(button13));
+    }
+
+    private void buttonCallBack(Button buttonPressed)
+    {
+        if (buttonPressed == button1)
+        {
+            //code for button 1
+            Debug.Log("Clicked: " + button1.name);
+
+            if (SpottedRayEgg.activeInHierarchy == false)
+            {
+                IncorrectSpottedRay.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (SpottedRayEgg.activeInHierarchy == true)
+            {
+                CorrectSpottedRay.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+
+        }
+
+        if (buttonPressed == button2)
+        {
+            //code for button 2
+            Debug.Log("Clicked: " + button2.name);
+
+            if (ThornbackRayEgg.activeInHierarchy == false)
+            {
+                IncorrectThornbackRay.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (ThornbackRayEgg.activeInHierarchy == true)
+            {
+                CorrectThornbackRay.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+        }
+
+        if (buttonPressed == button3)
+        {
+            //code for button 3
+            Debug.Log("Clicked: " + button3.name);
+
+            if (SmallEyedRayEgg.activeInHierarchy == false)
+            {
+                IncorrectSmallEyedRay.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (SmallEyedRayEgg.activeInHierarchy == true)
+            {
+                CorrectSmallEyedRay.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+
+        }
+
+        if (buttonPressed == button4)
+        {
+            //code for button 4
+            Debug.Log("Clicked: " + button4.name);
+
+            if (UndulateRayEgg.activeInHierarchy == false)
+            {
+                IncorrectUndulateRay.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (UndulateRayEgg.activeInHierarchy == true)
+            {
+                CorrectUndulateRay.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+        }
+
+        if (buttonPressed == button5)
+        {
+            //code for button 5
+            Debug.Log("Clicked: " + button5.name);
+
+            if (CuckooRayEgg.activeInHierarchy == false)
+            {
+                IncorrectCuckooRay.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }        
+            
+            if (CuckooRayEgg.activeInHierarchy == true)
+            {
+                CorrectCuckooRay.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+
+        }
+
+        if (buttonPressed == button6)
+        {
+            //code for button 6
+            Debug.Log("Clicked: " + button6.name);
+
+            if (BlondeRayEgg.activeInHierarchy == false)
+            {
+                IncorrectBlondeRay.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (BlondeRayEgg.activeInHierarchy == true)
+            {
+                CorrectBlondeRay.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+        }
+
+        if (buttonPressed == button7)
+        {
+            //code for button 7
+            Debug.Log("Clicked: " + button7.name);
+
+            if (StarrySkateEgg.activeInHierarchy == false)
+            {
+                IncorrectStarrySkate.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (StarrySkateEgg.activeInHierarchy == true)
+            {
+                CorrectStarrySkate.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+        }
+
+        if (buttonPressed == button8)
+        {
+            //code for button 8
+            Debug.Log("Clicked: " + button8.name);
+
+            if (BlueSkakeEgg.activeInHierarchy == false)
+            {
+                IncorrectBlueSkake.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (BlueSkakeEgg.activeInHierarchy == true)
+            {
+                CorrectBlueSkake.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+        }
+
+        if (buttonPressed == button9)
+        {
+            //code for button 9
+            Debug.Log("Clicked: " + button9.name);
+
+            if (FlapperSkakeEgg.activeInHierarchy == false)
+            {
+                IncorrectFlapperSkake.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (FlapperSkakeEgg.activeInHierarchy == true)
+            {
+                CorrectFlapperSkake.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+
+        }
+
+        if (buttonPressed == button10)
+        {
+            //code for button 10
+            Debug.Log("Clicked: " + button10.name);
+
+            if (WhiteSkakeEgg.activeInHierarchy == false)
+            {
+                IncorrectWhiteSkake.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (WhiteSkakeEgg.activeInHierarchy == true)
+            {
+                CorrectWhiteSkake.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+
+        }
+
+        if (buttonPressed == button11)
+        {
+            //code for button 11
+            Debug.Log("Clicked: " + button11.name);
+
+            if (SmallspottedCatsharkEgg.activeInHierarchy == false)
+            {
+                IncorrectSmallspottedCatshark.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (SmallspottedCatsharkEgg.activeInHierarchy == true)
+            {
+                CorrectSmallspottedCatshark.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+        }
+
+        if (buttonPressed == button12)
+        {
+            //code for button 12
+            Debug.Log("Clicked: " + button12.name);
+
+            if (NursehoundEgg.activeInHierarchy == false)
+            {
+                IncorrectNursehound.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (NursehoundEgg.activeInHierarchy == true)
+            {
+                CorrectNursehound.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+
+        }
+
+        if (buttonPressed == button13)
+        {
+            //code for button 13
+            Debug.Log("Clicked: " + button13.name);
+
+            if (BlackmouthCatsharkEgg.activeInHierarchy == false)
+            {
+                IncorrectBlackmouthCatshark.SetActive(true);
+                IncorrectGuesses = IncorrectGuesses + 1;
+            }
+
+            if (BlackmouthCatsharkEgg.activeInHierarchy == true)
+            {
+                CorrectBlackmouthCatshark.SetActive(true);
+                allIncorrectFalse();
+                allfalse();
+            }
+        }
+    }
+
+    
+    void allfalse()
+    {
+        SpottedRayEgg.SetActive(false);
+        ThornbackRayEgg.SetActive(false);
+        SmallEyedRayEgg.SetActive(false);
+        UndulateRayEgg.SetActive(false);
+        CuckooRayEgg.SetActive(false);
+        BlondeRayEgg.SetActive(false);
+        StarrySkateEgg.SetActive(false);
+        BlueSkakeEgg.SetActive(false);
+        FlapperSkakeEgg.SetActive(false);
+        WhiteSkakeEgg.SetActive(false);
+        SmallspottedCatsharkEgg.SetActive(false);
+        NursehoundEgg.SetActive(false);
+        BlackmouthCatsharkEgg.SetActive(false);
+
+    }
+
+    void allIncorrectFalse()
+    {
+        IncorrectSpottedRay.SetActive(false);
+        IncorrectThornbackRay.SetActive(false);
+        IncorrectSmallEyedRay.SetActive(false);
+        IncorrectUndulateRay.SetActive(false);
+        IncorrectCuckooRay.SetActive(false);
+        IncorrectBlondeRay.SetActive(false);
+        IncorrectStarrySkate.SetActive(false);
+        IncorrectBlueSkake.SetActive(false);
+        IncorrectFlapperSkake.SetActive(false);
+        IncorrectWhiteSkake.SetActive(false);
+        IncorrectSmallspottedCatshark.SetActive(false);
+        IncorrectNursehound.SetActive(false);
+        IncorrectBlackmouthCatshark.SetActive(false);
+    }
+}
