766f011596801bab8240d1ed64e903a3203aad11
Sulfion <2246364@swansea.ac.uk>
2023-05-11 18:03:17 +0100

    added documentation to script

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//rotates a gameobject on chosen axis
 public class Rotator : MonoBehaviour
 {
     public float rotateSpeed;
